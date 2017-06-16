07d9400990ececa85724d8199556536434479c81
Daniel Araujo <contact@daniel-araujo.pt>
2017-06-16 18:40:44 +0100

    Remove test cases of removed functions

--- src/tests/access-element.c
+++ src/tests/access-element.c
@@ -5,16 +5,16 @@
 int main(void)
 {
 	int expected_value = 123456789;
-	int retrieved_value = 0;
+	int *whats_there;
 
 	struct darr array;
 	darr_init(&array, sizeof(int));
 	darr_resize(&array, 1);
-	darr_set(&array, 0, &expected_value);
-	darr_copy(&array, 0, &retrieved_value);
+	whats_there = darr_address(&array, 0);
+	*whats_there = expected_value;
 
-	if (retrieved_value != expected_value) {
-		fprintf(stderr, "Failed to copy value.\n");
+	if (*whats_there != expected_value) {
+		fprintf(stderr, "Failed to set value.\n");
 		darr_deinit(&array);
 		return 1;
 	}
